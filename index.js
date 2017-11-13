ac67eddd42dc039a5284ad1b2aa45b1cf50f67fb
Your Name <you@example.com>
2017-11-12 19:42:46 -0800

    autoplay video and tab change mute video

--- index.js
+++ index.js
@@ -55,6 +55,16 @@ $( document ).ready(function() {
 			
 			$("#nav-music").attr("href", "#musicSection-ch");
 			$("#nav-acting").attr("href", "#actingSection-ch");
+			
+			var youtube = document.querySelectorAll( ".youtube" );
+			for (var i = 0; i < youtube.length; i++) {
+				if(youtube[i].firstChild.localName === "iframe"){
+					$( "#" + youtube[i].id+"-iframe" )[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+				}
+			}
+			
+			
+			
 		}else{
 			$( "#nav-music" ).html("Music");
 			$( "#nav-acting" ).html("Acting");
@@ -63,6 +73,49 @@ $( document ).ready(function() {
 			
 			$("#nav-music").attr("href", "#musicSection");
 			$("#nav-acting").attr("href", "#actingSection");
+			
+			
+			
+			
+		/*	
+		
+		
+						$("#video-panda-ch-youku").attr('src',''); 
+			$('#video-panda-ch-playbutton').show();
+		    $('#video-panda-ch-thumbnail').show();
+		    $('#video-panda-ch').show();
+            $("#video-panda-ch-youku").hide();
+			
+			
+						$("#video-xiangqin-ch-youku").attr('src',''); 
+			$('#video-xiangqin-ch-playbutton').show();
+		    $('#video-xiangqin-ch-thumbnail').show();
+		    $('#video-xiangqin-ch').show();
+            $("#video-xiangqin-ch-youku").hide();
+
+
+			
+*/
+
+
+		
+			var youku = document.querySelectorAll( ".youku" );
+	
+			for (var i = 0; i < youku.length; i++) {
+					
+				console.log("youku[i].id = " + youku[i].id);
+					
+				$('#' + youku[i].id + '-youku').attr('src',''); 
+				$('#' + youku[i].id + '-playbutton').show();
+				$('#' + youku[i].id + '-thumbnail').show();
+				$('#' + youku[i].id ).show();
+				$('#' + youku[i].id + '-youku').hide();
+
+			}
+			
+
+			
+	
 		}
 		 
 	});
@@ -78,6 +131,11 @@ $( document ).ready(function() {
 	document.getElementById("logo").style.width = (navibarH).toString() + "px";
 	document.getElementById("logo").style.left = (0).toString() + "px";
 	
+	
+	
+	
+	
+	
 	//reposition the "name and introduction" section.
 	//var profilePic1Div = document.getElementById('profilePic1Div');
 	//var profilePic1DivH = $('#profilePic1Div').outerHeight(true);
@@ -86,8 +144,8 @@ $( document ).ready(function() {
 	//document.getElementById("nameDiv").style.top = (profilePic1DivH * 0.3 ).toString() + "px";
 	
 	//change the video size
+	/*
 	var videoRow1Col1DivW = $('#videoRow1Col1').outerWidth(true);
-	//document.getElementById("video-panda").style.height = (videoRow1Col1DivW * 0.6 * 0.8).toString() + "px";
 	document.getElementById("video-panda").style.setProperty('--video-screen-height', ((videoRow1Col1DivW * 0.6 * 0.8).toString() + "px"));
 	document.getElementById("video-xiangqin").style.setProperty('--video-screen-height', ((videoRow1Col1DivW * 0.6 * 0.8).toString() + "px"));
 	document.getElementById("video-lilian").style.setProperty('--video-screen-height', ((videoRow1Col1DivW * 0.6 * 0.8).toString() + "px"));
@@ -105,7 +163,38 @@ $( document ).ready(function() {
 	
 	document.getElementById("video-mv-ch").style.setProperty('--video-screen-height', ((videoRow1Col1DivW * 0.6 * 0.8).toString() + "px"));
 	document.getElementById("video-zuichibi-ch").style.setProperty('--video-screen-height', ((videoRow1Col1DivW * 0.6 * 0.8).toString() + "px"));
+	*/
 	
+	
+	
+
+
+	
+	//$('#video-panda-ch-thumbnail').width(gridWidth);
+	//$('#video-panda-ch-thumbnail').height(gridWidth/1.77);
+	//$('#video-panda-ch-thumbnail').css({'width' : videoRow1Col1DivW.toString() + 'px' , 'height' : (videoRow1Col1DivW/1.77).toString() + 'px'});
+	//$('#video-xiangqin-ch-thumbnail').css({'width' : videoRow1Col1DivW.toString() + 'px' , 'height' : (videoRow1Col1DivW/1.77).toString() + 'px'});
+	
+	
+	var gridWidth = $('#gridWidth').outerWidth(true);
+	
+	console.log("gridWidth = " + gridWidth);
+	
+	var gridWidthChNew = gridWidth * 0.9;
+	
+	var youku = document.querySelectorAll( ".youku" );
+	
+	for (var i = 0; i < youku.length; i++) {	
+		document.getElementById(youku[i].id).style.setProperty('--iframe-width', ((gridWidthChNew).toString() + "px"));
+		document.getElementById(youku[i].id).style.setProperty('--iframe-height', ((gridWidthChNew / 1.77).toString() + "px"));
+
+		document.getElementById(youku[i].id + "-youku").style.setProperty('--iframe-width', ((gridWidthChNew).toString() + "px"));
+		document.getElementById(youku[i].id + "-youku").style.setProperty('--iframe-height', ((gridWidthChNew / 1.77).toString() + "px"));
+	}
+			
+			
+
+
 
 	
 	$("#ins").click(function(){
@@ -128,6 +217,118 @@ $( document ).ready(function() {
 		this.style.cursor = 'pointer'
 	});
 	
+	
+	
+	
+	var youtube = document.querySelectorAll( ".youtube" );
+	for (var i = 0; i < youtube.length; i++) {
+ 
+		// thumbnail image source.
+		var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/hqdefault.jpg"; 
+ 
+ 
+        var image = new Image();
+        image.src = source;
+        image.addEventListener( "load", function() {
+            youtube[ i ].appendChild( image );
+        }( i ) );
+		
+		
+		 youtube[i].addEventListener( "click", function() {
+ 
+        var iframe = document.createElement( "iframe" );
+ 
+            var a = youtube[i];
+            iframe.setAttribute( "id", this.id + "-iframe" );
+            iframe.setAttribute( "frameborder", "0" );
+            iframe.setAttribute( "allowfullscreen", "" );
+			iframe.setAttribute( "allowscriptaccess", "always" );
+           // iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+		   iframe.setAttribute( "src", "http://www.youtube.com/embed/"+ this.dataset.embed +"?enablejsapi=1&version=3&playerapiid=ytplayer&autoplay=1" );
+			
+
+            this.innerHTML = "";
+            this.appendChild(iframe);
+		} );
+	
+	}
+	
+	
+	
+	
+
+	
+	
+	
+	for (var i = 0; i < youku.length; i++) {
+
+		youku[i].addEventListener( "click", function() {
+			$('#' + this.id + '-playbutton').hide();
+			$('#' + this.id + '-thumbnail').hide();
+		    $('#' + this.id).hide();
+		   
+		    $('#' + this.id + '-youku').attr('src',$('#' + this.id + '-thumbnail').attr('data-video'));  
+		    $('#' + this.id + '-youku').show();
+       
+		} );
+	
+	}
+	
+	/*
+	 $('#video-panda-ch-playbutton').click(function(){
+		   $('#video-panda-ch-playbutton').hide();
+		   $('#video-panda-ch-thumbnail').hide();
+		   $('#video-panda-ch').hide();
+		   
+		   $("#video-panda-ch-youku").attr('src',$('#video-panda-ch-thumbnail').attr('data-video'));  
+		   $("#video-panda-ch-youku").show();
+		  
+	 });
+	 
+	 
+	 			setInterval(function() {
+					  
+					 $('#video-panda-youku-ch').click(); 
+					 var position = $('#video-panda-youku-ch').offset();
+					 document.elementFromPoint((position.left + 50).toString() + "px", (position.top + 50).toString() + "px").click();
+					  
+				}, 5000);
+
+ */
+
+	
+	
+	/*	
+	var youku = document.querySelectorAll( ".youku" );
+	for (var i = 0; i < youku.length; i++) {
+ 
+		// thumbnail image source.http://events.youku.com/global/api/video-thumb.php?vid=XNDk5OTE4MTAw
+		var source = "http://events.youku.com/global/api/video-thumb.php?vid="+ youku[i].dataset.embed; 
+ 
+ 
+        var image = new Image();
+        image.src = source;
+        image.addEventListener( "load", function() {
+            youku[ i ].appendChild( image );
+        }( i ) );
+		
+		
+		 youku[i].addEventListener( "click", function() {
+ 
+        var iframe = document.createElement( "iframe" );
+ 
+            iframe.setAttribute( "frameborder", "0" );
+            iframe.setAttribute( "allowfullscreen", "" );
+            iframe.setAttribute( "src", "http://player.youku.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+ 
+            this.innerHTML = "";
+            this.appendChild( iframe );
+		} );
+	
+	}
+
+	
+	*/
 	
 	
 	
