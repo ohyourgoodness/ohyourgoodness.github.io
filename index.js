d79c0f56559336fa654b97b8adedbdce6b9b6062
Your Name <you@example.com>
2017-12-20 07:59:57 -0800

    language option

--- index.js
+++ index.js
@@ -153,7 +153,144 @@ $( document ).ready(function() {
 		}
 
 	});
+	
+	
+	
+	$("#language-chinese").mouseover(function(){
+		this.style.cursor = 'pointer'
+	});
+	
+	$( "#language-chinese" ).click(function() {
+		
+		$( "#chinese-container" ).toggle();
+		$( "#english-container" ).toggle();
+		
+		if($('#chinese-container').is(':visible')) {
+			$( "#nav-me" ).html("我");
+			$( "#nav-music" ).html("音乐");
+			$( "#nav-acting" ).html("演戏");
+			$( "#nav-modeling" ).html("模特");
+			$( "#nav-language" ).html("English");
+			
+			
+			$("#nav-me").attr("href", "#meSection-ch");
+			$("#nav-music").attr("href", "#musicSection-ch");
+			$("#nav-acting").attr("href", "#actingSection-ch");
+			
+			var youtube = document.querySelectorAll( ".youtube" );
+			for (var i = 0; i < youtube.length; i++) {
+				if(youtube[i].firstChild.localName === "iframe"){
+					$( "#" + youtube[i].id+"-iframe" )[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+				}
+			}
+			
+		}else{
+			$( "#nav-me" ).html("About Me");
+			$( "#nav-music" ).html("Music");
+			$( "#nav-acting" ).html("Acting");
+			$( "#nav-modeling" ).html("Modeling");
+			$( "#nav-language" ).html("中文");
+			
+			$("#nav-me").attr("href", "#meSection");
+			$("#nav-music").attr("href", "#musicSection");
+			$("#nav-acting").attr("href", "#actingSection");
 
+			
+		/*	$("#video-panda-ch-youku").attr('src',''); 
+			$('#video-panda-ch-playbutton').show();
+		    $('#video-panda-ch-thumbnail').show();
+		    $('#video-panda-ch').show();
+            $("#video-panda-ch-youku").hide();			
+		*/
+
+
+			var youku = document.querySelectorAll( ".youku" );
+	
+			for (var i = 0; i < youku.length; i++) {
+					
+				console.log("youku[i].id = " + youku[i].id);
+					
+				$('#' + youku[i].id + '-youku').attr('src',''); 
+				$('#' + youku[i].id + '-playbutton').show();
+				$('#' + youku[i].id + '-thumbnail').show();
+				$('#' + youku[i].id ).show();
+				$('#' + youku[i].id + '-youku').hide();
+
+			}
+
+		}
+
+	});
+
+	
+	
+	
+	
+	$("#language-english").mouseover(function(){
+		this.style.cursor = 'pointer'
+	});
+	
+	$( "#language-english" ).click(function() {
+		
+		$( "#chinese-container" ).toggle();
+		$( "#english-container" ).toggle();
+		
+		if($('#chinese-container').is(':visible')) {
+			$( "#nav-me" ).html("我");
+			$( "#nav-music" ).html("音乐");
+			$( "#nav-acting" ).html("演戏");
+			$( "#nav-modeling" ).html("模特");
+			$( "#nav-language" ).html("English");
+			
+			
+			$("#nav-me").attr("href", "#meSection-ch");
+			$("#nav-music").attr("href", "#musicSection-ch");
+			$("#nav-acting").attr("href", "#actingSection-ch");
+			
+			var youtube = document.querySelectorAll( ".youtube" );
+			for (var i = 0; i < youtube.length; i++) {
+				if(youtube[i].firstChild.localName === "iframe"){
+					$( "#" + youtube[i].id+"-iframe" )[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+				}
+			}
+			
+		}else{
+			$( "#nav-me" ).html("About Me");
+			$( "#nav-music" ).html("Music");
+			$( "#nav-acting" ).html("Acting");
+			$( "#nav-modeling" ).html("Modeling");
+			$( "#nav-language" ).html("中文");
+			
+			$("#nav-me").attr("href", "#meSection");
+			$("#nav-music").attr("href", "#musicSection");
+			$("#nav-acting").attr("href", "#actingSection");
+
+			
+		/*	$("#video-panda-ch-youku").attr('src',''); 
+			$('#video-panda-ch-playbutton').show();
+		    $('#video-panda-ch-thumbnail').show();
+		    $('#video-panda-ch').show();
+            $("#video-panda-ch-youku").hide();			
+		*/
+
+
+			var youku = document.querySelectorAll( ".youku" );
+	
+			for (var i = 0; i < youku.length; i++) {
+					
+				console.log("youku[i].id = " + youku[i].id);
+					
+				$('#' + youku[i].id + '-youku').attr('src',''); 
+				$('#' + youku[i].id + '-playbutton').show();
+				$('#' + youku[i].id + '-thumbnail').show();
+				$('#' + youku[i].id ).show();
+				$('#' + youku[i].id + '-youku').hide();
+
+			}
+
+		}
+
+	});
 
 	//resize the logo size to fit the navibar, the original logo size must be set smaller than the default navibar size.
 	var navbar = document.getElementById('container-fluid');
